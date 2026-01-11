00fd1e808e77ab8fae364c046eb05d974d2aa690
Kosuri Nanda <kosurinanda9494@gmail.com>
2026-01-11 14:32:54 +0530

    added-mail-commit

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -4,7 +4,8 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true,
+        secure: false,
+        requireTLS: true,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
